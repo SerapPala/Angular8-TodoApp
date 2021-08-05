594cf2defd65a50f07fec4663fbd83ea69d5385b
Serap Pala <72605128+SerapPala@users.noreply.github.com>
2021-08-05 14:44:19 +0300

    Add files via upload

---
+++ src/app/model.ts
@@ -0,0 +1,23 @@
+export class Model {
+  user: string;
+  items: any;
+  constructor() {
+    this.user = 'Serap';
+    this.items = [
+        new TodoItem('Spor', true),
+        new TodoItem('Kahvaltı', false),
+        new TodoItem('Kitap Okumak', false),
+        new TodoItem('Sinema', false),
+    ];
+  }
+}
+export class TodoItem {
+    description: string;
+    action: boolean;
+
+    constructor(description: string, action: boolean) {
+    this.description = description;
+    this.action = action;
+    }
+
+}
